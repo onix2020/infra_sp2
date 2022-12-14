4f9dd706adedd41a2cfec8f135585a14633ec0c5
Alex J <onix8@mail.ru>
2022-12-15 01:03:36 +0500

    isort

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from api.views import (APIGetToken, APISignup, CategoryViewSet, CommentViewSet,
                        GenreViewSet, ReviewViewSet, TitleViewSet, UserViewSet)
 
