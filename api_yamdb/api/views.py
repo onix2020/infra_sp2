5b33760d6831a4654858dd5526b2a2b1a5776085
Alex J <onix8@mail.ru>
2022-12-16 01:42:27 +0500

    try 999

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,8 @@
+from api.serializers import (CategorySerializer, CommentSerializer,
+                             GenreSerializer, GetTokenSerializer,
+                             ReviewSerializer, SignUpSerializer,
+                             TitleCreateSerializer, TitleSerializer,
+                             UserSerializer)
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.db import IntegrityError
@@ -9,16 +14,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
-
-from .filters import TitleFilter
-from .permissions import AuthorStaffOrReadOnly, IsAdmin, IsAdminOrReadOnly
-from api.serializers import (CategorySerializer, CommentSerializer,
-                             GenreSerializer, GetTokenSerializer,
-                             ReviewSerializer, SignUpSerializer,
-                             TitleCreateSerializer, TitleSerializer,
-                             UserSerializer)
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
+from .filters import TitleFilter
+from .permissions import AuthorStaffOrReadOnly, IsAdmin, IsAdminOrReadOnly
+
+
+
 
 
 class UpdateDeleteViewSet(mixins.CreateModelMixin,
